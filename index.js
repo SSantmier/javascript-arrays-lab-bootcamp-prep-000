8d951607311db21f5e739985d1a69b99f7ce1cd2
Sara S <ssantmier@gmail.com>
2018-06-27 17:54:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ var kittens = ['Milo', 'Otis', 'Garfield'];
 
 function destructivelyAppendKitten(Ralph) {kittens.push(Ralph);  return kittens} 
 
+<<<<<<< HEAD
 function destructivelyPrependKitten(Bob) {kittens.unshift(Bob); kittens}
 
 function destructivelyRemoveLastKitten() {kittens.pop();}
 
 function destructivelyRemoveFirstKitten() {kittens.shift()}
+=======
+function destructivelyPrependKitten(Bob) {kittens.unshift(Bob); return kittens}
+
+function destructivelyRemoveLastKitten(kittens) {kittens.pop(); return kittens}
+
+function destructivelyRemoveFirstKitten(kittens) {kittens.shift(); return kittens}
+>>>>>>> 53cecbbcef5dac8c231b2ab3b33d1ef7efaa4e1f
 
 function appendKitten(Broom) {return [...kittens, Broom]}
 
